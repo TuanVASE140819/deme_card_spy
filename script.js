d5f8ccb8d299853d65a25ee4f883d8e8dae73cc3
Vu Anh Tuan <tuanvuanhse140819@gmail.com>
2023-12-11 10:15:40 +0700

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+// // Code goes here
+
+// $(document).ready(function () {
+//   $('[data-spy="scroll"]').each(function () {
+//     var $spy = $(this).scrollspy("refresh");
+//   });
+
+//   $("#target_nav").on("activate.bs.scrollspy", function () {
+//     var activeTab = $(".nav-tabs li.active a");
+//     activeTab.parent().removeClass("active");
+//     activeTab.tab("show");
+//   });
+// });
